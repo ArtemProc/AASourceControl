5e696f1b0482f4421e1c5aad5e58f9e273a68220
ArtemProc <asprocenko@gmail.com>
2016-03-20 14:44:41 +0300

    Created Start-InlineScript.ps1 in AzureAutomation.

---
+++ Start-InlineScript.ps1
@@ -0,0 +1,17 @@
+workflow Start-InlineScript
+{
+	$os = "XP"
+ 
+	switch ($os) 
+    {
+ 
+		"XP"        {"Time to upgrade"}
+        
+		"Windows 7"  {"OK - but not the latest"}        
+ 
+ 		"Windows8.1"  {"OK - but not the latest"}
+ 
+		"Windows 10" {"Latest and greatest"}
+ 
+	}
+}
